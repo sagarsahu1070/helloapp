5ebbca0f3284d633474d4cde15cf7056407c4a6b
sagarsahu1070 <sagarsahu1070@gmail.com>
2026-03-17 08:40:51 +0530

    UC4 DONE

---
+++ helloapp.java
@@ -0,0 +1,16 @@
+class helloapp {
+    public static void main(String args[]) {
+
+        if(args.length > 0) {
+            System.out.print("Hello ");
+            
+            for(int i = 0; i < args.length; i++) {
+                System.out.print(args[i] + " ");
+            }
+        } 
+        else {
+            System.out.println("Hello ");
+        }
+
+    }
+}
